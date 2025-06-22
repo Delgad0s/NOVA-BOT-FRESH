313fcbc37bd6ca29b5c7ac8cb3f0a78b593000f0
Delgad0s <luisjairdelgadoguerrero@hotmail.com>
2025-06-22 15:42:33 -0600

    Desactivo run_polling para evitar conflicto con instancia local

--- nova_bot.py
+++ nova_bot.py
@@ -37,7 +37,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    app.run_polling()
+    # app.run_polling()
 
 if __name__ == "__main__":
     main()
